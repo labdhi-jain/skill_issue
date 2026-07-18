af21b50fc84838e34ab8c04d7cc3bdb3274fac64
labdhi-jain <jainlabdhi1405@gmail.com>
2026-07-18 19:54:19 +0530

    Update SMTP password in .env

--- server/mailer.js
+++ server/mailer.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
   secure: false, // TLS
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    pass: (process.env.SMTP_PASS || '').replace(/\s+/g, ''),
   },
 });
 
